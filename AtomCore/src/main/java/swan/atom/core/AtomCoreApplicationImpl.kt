ccb4d06c37aa66ebd6bc868efa442b4ffb313292
stephen.guo <stephen.guo@qunar.com>
2018-03-31 16:59:50 +0800

    新增recy

--- AtomCore/src/main/java/swan/atom/core/AtomCoreApplicationImpl.kt
+++ AtomCore/src/main/java/swan/atom/core/AtomCoreApplicationImpl.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import com.github.ajalt.timberkt.Timber
 import com.joanzapata.iconify.Iconify
 import swan.atom.core.base.SwanAtomApplicationImpl
-import swan.biz.koala.AtomCoreIconifyModule
+import swan.atom.core.icon.AtomCoreIconifyModule
 import java.lang.ref.WeakReference
 
 /**
