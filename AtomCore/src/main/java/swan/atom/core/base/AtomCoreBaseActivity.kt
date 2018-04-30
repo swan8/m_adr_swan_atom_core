56e581d6d57abf7f4531bae06dd62c3351d031c8
stephen.guo <stephen.guo@qunar.com>
2018-05-01 00:39:59 +0800

    new search activity

--- AtomCore/src/main/java/swan/atom/core/base/AtomCoreBaseActivity.kt
+++ AtomCore/src/main/java/swan/atom/core/base/AtomCoreBaseActivity.kt
@@ -1,16 +1,33 @@
 package swan.atom.core.base
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.gyf.barlibrary.ImmersionBar
+import swan.atom.core.R
 
 /**
  * Created by stephen on 13/03/2018.
  */
 open class AtomCoreBaseActivity: AppCompatActivity(), View.OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    var AtomCoreBaseActivity.layoutResId
+    get(): Int {
+        return 0
+    }
+    set(value) {
+        setContentView(value)
+    }
+
+    var AtomCoreBaseActivity.immersionBar: ImmersionBar
+    get(): ImmersionBar {
+        return ImmersionBar
+                .with(this)
+                .fitsSystemWindows(true)
+                .statusBarColor(R.color.colorPrimary)
+                .statusBarDarkFont(false)
+    }
+    private set(value) {
+
     }
 
     override fun onClick(v: View?) {
--- AtomCore/src/main/java/swan/atom/core/base/AtomCoreBaseToolbarActivity.kt
+++ AtomCore/src/main/java/swan/atom/core/base/AtomCoreBaseToolbarActivity.kt
@@ -1,7 +1,5 @@
 package swan.atom.core.base
 
-import com.gyf.barlibrary.ImmersionBar
-import swan.atom.core.R
 import swan.atom.core.ui.Cirrus
 
 /**
@@ -13,31 +11,11 @@ open class AtomCoreBaseToolbarActivity: AtomCoreBaseActivity() {
 
     var cirrus: Cirrus? = null
 
-    var AtomCoreBaseToolbarActivity.layoutResId
-    get(): Int {
-        return 0
-    }
-    set(value) {
-        setContentView(value)
-    }
-
     var AtomCoreBaseToolbarActivity.cirrusResId
     get(): Int {
         return 0
     }
     set(value) {
         cirrus = findViewById<Cirrus>(value)
-    }
-
-    var AtomCoreBaseToolbarActivity.immersionBar: ImmersionBar
-    get(): ImmersionBar {
-        return ImmersionBar
-                .with(this)
-                .fitsSystemWindows(true)
-                .statusBarColor(R.color.colorPrimary)
-                .statusBarDarkFont(false)
-    }
-    private set(value) {
-
     }
 }
